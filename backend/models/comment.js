34806c7f150c4b0642ba996d9c6d5e45f22b9e31
Giovanne Rocha Vieira <rocha.giovanne07@gmail.com>
2025-07-31 13:53:20 -0300

    Delete backend directory

--- backend/models/comment.js
+++
@@ -1,7 +0,0 @@
-const { DataTypes } = require('sequelize');
-
-module.exports = (sequelize) =>
-  sequelize.define('Comment', {
-    id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
-    message: { type: DataTypes.TEXT, allowNull: false }
-  });
